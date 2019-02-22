a6d52a166a6a009f0effa60fc73882ba0142b8ee
Baruch Hashem <barukhashem@gmail.com>
2019-02-21 21:55:13 -0700

    adding code

---
+++ app/data/friends.js
@@ -0,0 +1,371 @@
+// Friends (DATA)
+// =============================================================
+var users = [
+    {
+        "name": "Colin",
+        "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSGL4_G5Yx9MPphnz_DKGSgZ4jyYbol5ZGR-N8XItSngZGreN8Z",
+        "scores": [
+            "5",
+            "1",
+            "4",
+            "4",
+            "5",
+            "1",
+            "2",
+            "5",
+            "4",
+            "1"
+        ]
+    },
+    {
+        "name": "Jane",
+        "photo": "https://pbs.twimg.com/profile_images/1042562535277428736/f4i9bUJ5_400x400.jpg",
+        "scores": [
+            "5",
+            "2",
+            "4",
+            "3",
+            "5",
+            "2",
+            "2",
+            "5",
+            "3",
+            "1"
+        ]
+    },
+    {
+        "name": "Zoe",
+        "photo": "https://pmcdeadline2.files.wordpress.com/2016/01/zoe-saldana-01-600x800.jpg?crop=0px%2C6px%2C576px%2C386px&resize=446%2C299",
+        "scores": [
+            "3",
+            "2",
+            "5",
+            "3",
+            "2",
+            "1",
+            "2",
+            "1",
+            "4",
+            "4"
+        ]
+    },
+    {
+        "name": "Julian",
+        "photo": "https://s.yimg.com/ny/api/res/1.2/h.gIFhQMJwuzxZGijVdJ3A--~A/YXBwaWQ9aGlnaGxhbmRlcjtzbT0xO3c9MzAwO2g9Mzk0O2lsPXBsYW5l/http://media.zenfs.com/en/blogs/ymusic-stop-the-presses/3813-JulianLennon.jpg",
+        "scores": [
+            "5",
+            "1",
+            "4",
+            "4",
+            "5",
+            "1",
+            "2",
+            "5",
+            "4",
+            "1"
+        ]
+    },
+    {
+        "name": "James",
+        "photo": "https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTE5NTU2MzE2MDU5OTYwODQz/james-hetfield-278912-1-402.jpg",
+        "scores": [
+            "4",
+            "2",
+            "5",
+            "1",
+            "3",
+            "2",
+            "2",
+            "1",
+            "3",
+            "2"
+        ]
+    },
+    {
+        "name": "Ryan",
+        "photo": "https://statici.behindthevoiceactors.com/behindthevoiceactors/_img/actors/ryan-mccluskey-1.92.jpg",
+        "scores": [
+            "5",
+            "2",
+            "2",
+            "2",
+            "4",
+            "1",
+            "3",
+            "2",
+            "5",
+            "5"
+        ]
+    },
+    {
+        "name": "Jimmy",
+        "photo": "https://pbs.twimg.com/profile_images/639214960049000449/lNCRC-ub.jpg",
+        "scores": [
+            "3",
+            "3",
+            "4",
+            "2",
+            "2",
+            "1",
+            "3",
+            "2",
+            "2",
+            "3"
+        ]
+    },
+    {
+        "name": "Johnny",
+        "photo": "https://upload.wikimedia.org/wikipedia/commons/thumb/0/04/William_Zabka_2018.jpg/220px-William_Zabka_2018.jpg",
+        "scores": [
+            "4",
+            "3",
+            "4",
+            "1",
+            "5",
+            "2",
+            "5",
+            "3",
+            "1",
+            "4"
+        ]
+    },
+    {
+        "name": "Katie",
+        "photo": "https://whyy.org/wp-content/uploads/2017/07/katie-meyer-staff-photo-300-1.jpg",
+        "scores": [
+            "4",
+            "4",
+            "2",
+            "3",
+            "2",
+            "2",
+            "3",
+            "2",
+            "4",
+            "5"
+        ]
+    },
+    {
+        "name": "Jason;",
+        "photo": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Jason_Momoa_Supercon_2014.jpg/170px-Jason_Momoa_Supercon_2014.jpg;",
+        "scores": [
+            "5",
+            "5",
+            "3",
+            "4",
+            "4",
+            "2",
+            "3",
+            "3",
+            "5",
+            "1"
+        ]
+    },
+    {
+        "name": "Josh",
+        "photo": "https://m.media-amazon.com/images/M/MV5BMTQ1MzYyMjQ0Nl5BMl5BanBnXkFtZTcwMTA0ODkyMg@@._V1_UX214_CR0,0,214,317_AL_.jpg",
+        "scores": [
+            "2",
+            "2",
+            "2",
+            "1",
+            "3",
+            "4",
+            "3",
+            "3",
+            "3",
+            "3"
+        ]
+    },
+    {
+        "name": "Paula",
+        "photo": "https://m.media-amazon.com/images/M/MV5BMTQ0Njk3ODYzMF5BMl5BanBnXkFtZTgwNjU5MTkxNzE@._V1_UY317_CR131,0,214,317_AL_.jpg",
+        "scores": [
+            "3",
+            "3",
+            "1",
+            "2",
+            "4",
+            "2",
+            "1",
+            "3",
+            "3",
+            "4"
+        ]
+    },
+    {
+        "name": "David",
+        "photo": "http://img2.wikia.nocookie.net/__cb20140303035016/filmguide/images/f/fb/David_Duchovny.jpg",
+        "scores": [
+            "3",
+            "3",
+            "5",
+            "2",
+            "5",
+            "2",
+            "1",
+            "3",
+            "3",
+            "4"
+        ]
+    },
+    {
+        "name": "Donny",
+        "photo": "https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTE4MDAzNDEwODAyNjczMTY2/donnie-wahlberg-17121732-1-402.jpg",
+        "scores": [
+            "3",
+            "3",
+            "1",
+            "2",
+            "4",
+            "2",
+            "1",
+            "3",
+            "1",
+            "4"
+        ]
+    },
+    {
+        "name": "Sherry",
+        "photo": "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+        "scores": [
+            "3",
+            "3",
+            "1",
+            "2",
+            "4",
+            "2",
+            "1",
+            "3",
+            "2",
+            "4"
+        ]
+    },
+    {
+        "name": "Jenny",
+        "photo": "https://24smi.org/public/media/235x307/celebrity/2018/10/11/zyvfaeipw05k-jenny-slate.jpg",
+        "scores": [
+            "2",
+            "2",
+            "2",
+            "2",
+            "3",
+            "3",
+            "2",
+            "3",
+            "3",
+            "3"
+        ]
+    },
+    {
+        "name": "Chantelle",
+        "photo": "https://mortgageoutlet.ca/wp-content/uploads/2016/06/Allison.png",
+        "scores": [
+            "3",
+            "4",
+            "3",
+            "4",
+            "1",
+            "3",
+            "1",
+            "2",
+            "5",
+            "1"
+        ]
+    },
+    {
+        "name": "Chris",
+        "photo": "https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Chris_Pratt_2018.jpg/220px-Chris_Pratt_2018.jpg",
+        "scores": [
+            "3",
+            "2",
+            "3",
+            "4",
+            "1",
+            "5",
+            "1",
+            "2",
+            "2",
+            "1"
+        ]
+    },
+    {
+        "name": "Karen",
+        "photo": "https://upload.wikimedia.org/wikipedia/commons/thumb/0/01/Karen_Gillan_-_Florida_Supercon_2017_%2836263084666%29.jpg/250px-Karen_Gillan_-_Florida_Supercon_2017_%2836263084666%29.jpg",
+        "scores": [
+            "3",
+            "4",
+            "1",
+            "4",
+            "1",
+            "2",
+            "1",
+            "2",
+            "1",
+            "1"
+        ]
+    },
+    {
+        "name": "Paulina",
+        "photo": "https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTIwNjA4NjM0MjY4Mzg2ODI4/foxs-the-x-factor-season-finale.jpg",
+        "scores": [
+            "3",
+            "4",
+            "3",
+            "4",
+            "1",
+            "3",
+            "1",
+            "2",
+            "5",
+            "1"
+        ]
+    },
+    {
+        "name": "Marina",
+        "photo": "https://images.bwwstatic.com/columnpic10/2BB61648F-9711-70EE-57D2795265A9EE75.jpg",
+        "scores": [
+            "3",
+            "4",
+            "3",
+            "4",
+            "1",
+            "3",
+            "1",
+            "2",
+            "1",
+            "1"
+        ]
+    },
+    {
+        "name": "Dave",
+        "photo": "https://pixel.nymag.com/imgs/daily/vulture/2018/08/06/06-dave-bautista.w330.h330.jpg",
+        "scores": [
+            "2",
+            "1",
+            "2",
+            "1",
+            "3",
+            "4",
+            "3",
+            "4",
+            "3",
+            "5"
+        ]
+    }
+    // {
+    //   name: "Brian",
+    //   outgoingPerson: 3,
+    //   keepWord: 5,
+    //   likeKids: 4,
+    //   easilyUpset: 3,
+    //   familyHarmony: 5,
+    //   handleStress: 2,
+    //   sportsFan: 1,
+    //   considerOthers: 5,
+    //   likeAnimals: 4,
+    //   handleConflict: 2
+    // }
+];
+
+module.exports = friends;
--- server.js
+++ server.js
@@ -3,6 +3,8 @@
 var express = require("express");
 var path = require("path");
 
+var friends = require("./app/data/friends");
+// console.log(friends);
 // Sets up the Express App
 // =============================================================
 var app = express();
@@ -14,31 +16,21 @@ var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Friends (DATA)
-// =============================================================
-var friends = [
-  {
-    routeName: "yoda",
-    name: "Yoda",
-    role: "Jedi Master",
-    age: 900,
-    forcePoints: 2000
-  },
-  {
-    routeName: "darthmaul",
-    name: "Darth Maul",
-    role: "Sith Lord",
-    age: 200,
-    forcePoints: 1200
-  },
-  {
-    routeName: "obiwankenobi",
-    name: "Obi Wan Kenobi",
-    role: "Jedi Master",
-    age: 55,
-    forcePoints: 1350
-  }
-];
+var user1 = users[0].scores;
+var user2 = users[1].scores;
+var user3 = users[2].scores;
+console.log(user1, user2);
+var totalDifference = 0;
+
+for (var i = 0; i < user1.length; i++) {
+  // console.log(user1[i], user2[i]);
+  var num1 = user1[i];
+  var num2 = user2[i];
+  var singleDifference = Math.abs(num1 - num2);
+  console.log(singleDifference);
+  totalDifference = singleDifference + totalDifference;
+}
+console.log(totalDifference);
 
 // Routes
 // =============================================================
@@ -52,21 +44,21 @@ app.get("/survey", function (req, res) {
   res.sendFile(path.join(__dirname, "survey.html"));
 });
 
-// Displays all friends
+// Displays all users
 app.get("/api/friends", function (req, res) {
-  return res.json(friends);
+  return res.json(users);
 });
 
-// Displays a single friend, or returns false
+// Displays a single user, or returns false
 // localhost:PORT/api/characters/yoda
 app.get("/api/friends/:friends", function (req, res) {
-  var chosen = req.params.friend;
+  var chosen = req.params.user;
 
   console.log(chosen);
 
-  for (var i = 0; i < friends.length; i++) {
-    if (chosen === friends[i].routeName) {
-      return res.json(friends[i]);
+  for (var i = 0; i < users.length; i++) {
+    if (chosen === users[i].routeName) {
+      return res.json(users[i]);
     }
   }
 
@@ -77,17 +69,17 @@ app.get("/api/friends/:friends", function (req, res) {
 app.post("/api/friends", function (req, res) {
   // req.body hosts is equal to the JSON post sent from the user
   // This works because of our body parsing middleware
-  var newFriend = req.body;
+  var newUser = req.body;
 
   // Using a RegEx Pattern to remove spaces from newFriend
   // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-  newFriend.routeName = newFriend.name.replace(/\s+/g, "").toLowerCase();
+  newUser.routeName = newUser.name.replace(/\s+/g, "").toLowerCase();
 
-  console.log(newFriend);
+  console.log(newUser);
 
-  characters.push(newFriend);
+  friends.push(newUser);
 
-  res.json(newFriend);
+  res.json(newUser);
 });
 
 // Starts the server to begin listening
@@ -95,3 +87,4 @@ app.post("/api/friends", function (req, res) {
 app.listen(PORT, function () {
   console.log("App listening on PORT " + PORT);
 });
+
